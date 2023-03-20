6d7be966ad57c26a380586fec297f00b42850ece
Lance Pioch <git@lance.sh>
2023-03-20 17:21:03 -0400

    Add steam api basics

---
+++ app/Http/Integrations/Steam/SteamConnector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Integrations\Steam;
+
+use Saloon\Http\Connector;
+use Saloon\Traits\Plugins\AcceptsJson;
+
+class SteamConnector extends Connector
+{
+    use AcceptsJson;
+
+    /**
+     * The Base URL of the API
+     *
+     * @return string
+     */
+    public function resolveBaseUrl(): string
+    {
+        return 'https://api.steampowered.com';
+    }
+
+    /**
+     * Default headers for every request
+     *
+     * @return string[]
+     */
+    protected function defaultHeaders(): array
+    {
+        return [];
+    }
+
+    /**
+     * Default HTTP client options
+     *
+     * @return string[]
+     */
+    protected function defaultConfig(): array
+    {
+        return [];
+    }
+}
