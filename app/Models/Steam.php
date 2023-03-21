037846b64ac0c463ff85f21303e61a7918154d7b
Lance Pioch <git@lance.sh>
2023-03-21 16:02:19 -0400

    Add scout and search

--- app/Models/Steam.php
+++ app/Models/Steam.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Steam extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $fillable = ['app_id', 'name'];
+
+    public function searchableAs()
+    {
+        return 'steamapps_index';
+    }
+
+    public function getScoutKeyName()
+    {
+        return 'appid';
+    }
 }
