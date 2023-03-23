156542cf82faf14fe42c9a61bbfed8fbd0d9af69
Lance Pioch <git@lance.sh>
2023-03-23 02:00:11 -0400

    Add basic component for table

---
+++ app/Http/Livewire/ListGames.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Row;
+use App\Models\Steam;
+use Filament\Tables;
+use Filament\Tables\Columns\SelectColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+
+class ListGames extends Component  implements Tables\Contracts\HasTable
+{
+    use Tables\Concerns\InteractsWithTable;
+
+    protected function getTableQuery(): Builder
+    {
+        return Row::query();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            // Tables\Columns\ImageColumn::make('author.avatar')->size(40)->circular(),
+            // Tables\Columns\TextColumn::make('author.name'),
+            /*Tables\Columns\BadgeColumn::make('status')
+                ->colors([
+                    'danger' => 'draft',
+                    'warning' => 'reviewing',
+                    'success' => 'published',
+                ]), // */
+
+            Tables\Columns\TextColumn::make('id'),
+            Tables\Columns\TextColumn::make('name'),
+            // Tables\Columns\TextColumn::make('key'),
+            Tables\Columns\TextColumn::make('acquired_at'),
+            Tables\Columns\IconColumn::make('used')->boolean()
+                ->trueColor('primary')
+                ->falseColor('warning'),
+            Tables\Columns\TextColumn::make('source'),
+            Tables\Columns\TextColumn::make('price'),
+            // Tables\Columns\TextColumn::make('sent_to'),
+            // Tables\Columns\TextColumn::make('sent_at'),
+            Tables\Columns\TextInputColumn::make('appid'),
+            Tables\Columns\TextInputColumn::make('appid'),
+            SelectColumn::make('appid')->options(function (...$params) {
+                $row = $params[2]; /** @var Row $row */
+                $options = $row->steam ? [$row->steam->appid => $row->steam->name] : [];
+                $closest = Steam::search($row->name)->take(3)->get();
+                $options += $closest->mapWithKeys(fn (Steam $steam) => [$steam->appid => $steam->name])->all();
+
+                return $options;
+            }),
+            Tables\Columns\TextColumn::make('steam.name'),
+        ];
+    }
+
+    protected function getTableFilters(): array
+    {
+        return [
+            Tables\Filters\Filter::make('associated')
+                ->query(fn (Builder $query): Builder => $query->whereNot('appid', '', 0)),
+            Tables\Filters\Filter::make('unassociated')
+                ->query(fn (Builder $query): Builder => $query->whereNull('appid')->orWhere('appid', '')),
+        ];
+    }
+
+    public function render()
+    {
+        return view('livewire.list-games');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,12 @@
         </h2>
     </x-slot>
 
+
+    <div class="py-12">
+        <h1>Test</h1>
+        <livewire:list-games></livewire:list-games>
+    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
