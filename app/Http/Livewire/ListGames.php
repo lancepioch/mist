8d58eb9b66dc6fe8141c1f4b8aba5df9d62dde3d
Lance Pioch <git@lance.sh>
2023-05-21 18:49:02 -0400

    Update columns

--- app/Http/Livewire/ListGames.php
+++ app/Http/Livewire/ListGames.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire;
 
 use App\Models\Row;
-use App\Models\Steam;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
@@ -20,17 +19,20 @@ class ListGames extends Component implements Tables\Contracts\HasTable
     protected function getTableColumns(): array
     {
         return [
-            Tables\Columns\TextColumn::make('id')->hidden(),
-            Tables\Columns\ImageColumn::make('steam.banner')->width(184)->height(69),
+            Tables\Columns\TextColumn::make('id')->sortable()->hidden(),
+            Tables\Columns\ImageColumn::make('steam.banner')->width(184)->height(69)->label(''),
             Tables\Columns\TextColumn::make('name')->wrap()->sortable()->searchable()
                 ->url(fn (...$params) => "https://steamcommunity.com/app/{$params[2]->appid}"),
             Tables\Columns\TextColumn::make('acquired_at')->date()
                 ->sortable(query: fn (Builder $query, string $direction): Builder => $query->orderBy('acquired_at', $direction)),
             Tables\Columns\IconColumn::make('available')->boolean(),
             Tables\Columns\TextColumn::make('source')->wrap()->toggleable(true, true),
-            Tables\Columns\TextColumn::make('price')->toggleable(true, true),
+            Tables\Columns\TextColumn::make('price')->sortable()
+                ->toggleable(true, true),
             Tables\Columns\TextColumn::make('sent_at')->hidden(),
-            Tables\Columns\SelectColumn::make('appid')->options(Row::appidOptions(...))->toggleable(true, true),
+            Tables\Columns\SelectColumn::make('appid')->label('Steam Pairing')->sortable()
+                ->options(Row::appidOptions(...))
+                ->toggleable(true, true),
             Tables\Columns\TextColumn::make('steam.name')->wrap()->hidden(),
         ];
     }
