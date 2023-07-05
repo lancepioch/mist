69243f77cc805764bde464d7c1ae01c5d4c0affc
Lance Pioch <git@lance.sh>
2023-07-05 09:57:14 -0400

    Revert text colors

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
         @stack('scripts')
     </head>
     <body>
-        <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="font-sans antialiased">
             {{ $slot }}
         </div>
     </body>
